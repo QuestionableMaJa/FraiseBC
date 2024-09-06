76348572d8f60591961ae250fb0b3d3dd7dbc4aa
MaJaNames <42037799+MaJaNames@users.noreply.github.com>
2024-09-06 19:49:13 +1100

    Create FraiseBCloader.user.js

---
+++ FraiseBCloader.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name FraiseScript
+// @namespace https://www.bondageprojects.com/
+// @version 1.0.0
+// @description A various silly scripts that Fraise makes
+// @author MaJaNamesuu
+// @match http://localhost:*/*
+// @match https://bondageprojects.elementfx.com/*
+// @match https://bondage-europe.com/*
+// @match https://www.bondage-europe.com/*
+// @match https://bondageprojects.com/*
+// @match https://www.bondageprojects.com/*
+// @grant none
+// @run-at document-end
+// ==/UserScript==
+
+(function () {
+  "use strict";
+  var script = document.createElement("script");
+  script.setAttribute("crossorigin", "anonymous");
+  script.src = "https://lunarkitsunify.github.io/MoonCEBC/MoonCEBC.js";
+  document.head.appendChild(script);
+})();
