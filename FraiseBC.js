4da5bb61298d87b28ffd64963cd223e236b3402a
MaJaNames <42037799+MaJaNames@users.noreply.github.com>
2024-09-06 19:50:06 +1100

    Create FraiseBC.js

---
+++ FraiseBC.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name FraiseScript
+// @namespace https://www.bondageprojects.com/
+// @version 1.0.0
+// @description A various silly scripts that Fraise makes
+// @author MaJaNamesuu
+// @match http://localhost:*/*
+// @match https://bondageprojects.elementfx.com/*
+// @match https://bondage-europe.com/*
+// @match https://www.bondage-europe.com/*
+// @match https://bondageprojects.com/*
+// @match https://www.bondageprojects.com/*
+// @grant none
+// @run-at document-end
+// ==/UserScript==
