d4bcf54e3d95ec68bf21306773b2c6d5f46cd10f
MaJaNames <42037799+MaJaNames@users.noreply.github.com>
2024-11-12 19:11:15 +1100

    Update FraiseBCloader.user.js

--- FraiseBCloader.user.js
+++ FraiseBCloader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name FraiseBC
 // @namespace https://www.bondageprojects.com/
-// @version 1.0.4
+// @version 1.0.5
 // @description A very silly script made by MaJaNamesuu
 // @author MaJaNamesuu
 // @match http://localhost:*/*
